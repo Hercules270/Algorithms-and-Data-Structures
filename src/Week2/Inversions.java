76b05d55a820f05afbb1458708fe8b95d8ae875d
Hercules270 <ibezh17@freeuni.edu.ge>
2020-10-13 01:55:58 +0400

    Started Implemented counting inversions

---
+++ src/Week2/Inversions.java
@@ -0,0 +1,101 @@
+package Week2;
+
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+class Inversions {
+
+    private static final int BASE_CASE = 1;
+
+
+    private static int sortAndCountInversions(int[] array, int length) {
+        if(array.length == 1) {
+            return 0;
+        }
+
+
+        int[] left = new int[length/2];
+        int[] right = new int[length/2 + length%2];
+
+        System.arraycopy(array, 0, left, 0, length/2);
+        System.arraycopy(array, length/2, right, 0, length/2 + length%2);
+
+        int leftInv = sortAndCountInversions(left, length/2);
+        int rightInv = sortAndCountInversions(right, length/2 + length % 2);
+
+        int splitInv = mergeAndCountSplit(left, right, array, length);
+
+
+        return leftInv + rightInv + splitInv;
+
+    }
+
+    private static int mergeAndCountSplit(int[] left, int[] right, int[] array, int length) {
+        // if(left.length == 3) {
+        // System.out.println("Left " + Arrays.toString(left));
+
+        // }
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int count = 0;
+        for(;k < length; k++) {
+            if(i == left.length || j == right.length) break;
+            if(left[i] <= right[j]) {
+                array[k] = left[i++];
+
+            } else {
+                array[k] = right[j++];
+                count += (left.length - i);
+            }
+        }
+
+
+        while(i<left.length) {
+            array[k++] = left[i++];
+            //count++;
+        }
+
+        while(j < right.length) {
+            array[k++] = right[j++];
+        }
+
+        return count;
+    }
+
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strInts;
+        int[] array = new int[100000];
+
+        for(int i=0; i<100000; i++) {
+            array[i] = i;
+
+        }
+        strInts = reader.readLine().split("\\s");
+        System.out.println("Reading ended successfully...");
+   /* while(true) {
+      strInts = reader.readLine().split("\\s");
+	  System.out.println("Reading ended successfully...");
+      int[] array = new int[strInts.length];
+      for (int i=0; i < strInts.length; i++) {
+       array[i] = Integer.parseInt(strInts[i]);
+      }
+      */
+        System.out.println("Copying ended succesfully...");
+
+        System.out.println("Number of inversions is " + sortAndCountInversions(array, array.length));
+        // System.out.println(Arrays.toString(array));
+
+    }
+
+}

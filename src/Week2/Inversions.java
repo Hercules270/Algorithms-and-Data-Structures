f21c2eca4ded10c201a1ad5d43cfd8624ec0053d
Hercules270 <ibezh17@freeuni.edu.ge>
2020-10-14 18:33:50 +0400

    Minor changes

--- src/Week2/Inversions.java
+++ src/Week2/Inversions.java
@@ -1,11 +1,8 @@
 package Week2;
 
 
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
